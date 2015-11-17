aee7d9e9df980fd8b344c709c4609400c53dba7d
Ad van der Veer <advanderveer@gmail.com>
2015-11-17 09:58:06 +0100

    basic bittorrent tracker implementation

--- Makefile
+++ Makefile
@@ -11,3 +11,4 @@ deps:
 	rm -fr vendor
 	git clone https://github.com/codegangsta/cli.git vendor/github.com/codegangsta/cli; cd vendor/github.com/codegangsta/cli; git checkout a65b733b303f0055f8d324d805f393cd3e7a7904
 	git clone https://github.com/golang/net.git vendor/golang.org/x/net; cd vendor/golang.org/x/net; git checkout c764672d0ee39ffd83cfcb375804d3181302b62b
+	git clone https://github.com/anacrolix/torrent vendor/github.com/anacrolix/torrent; cd vendor/github.com/anacrolix/torrent; git checkout b10c9921710fb770823d4bdb2025601c8a4daa17
--- services/exchange.go
+++ services/exchange.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"bytes"
 	"crypto/rand"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -11,10 +13,19 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+
+	"github.com/anacrolix/torrent/bencode"
 )
 
 func NewDeluge(ip net.IP) (Exchange, error) {
 	return &delugeProcess{ip: ip}, nil
+}
+
+type peer struct {
+	PeerID string `bencode:"peer id"`
+	IP     string `bencode:"ip"`
+	Port   string `bencode:"port"`
 }
 
 type Exchange interface {
@@ -32,7 +43,7 @@ type delugeProcess struct {
 }
 
 func (d *delugeProcess) Start() error {
-	cmd := exec.Command("deluged", "-d", "-L", "debug", "-i", d.ip.String())
+	cmd := exec.Command("deluged", "-d", "-L", "info", "-i", d.ip.String())
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Start()
@@ -42,6 +53,32 @@ func (d *delugeProcess) Start() error {
 
 	d.Process = cmd.Process
 	return nil
+}
+
+func (d *delugeProcess) writeCompactPeers(b *bytes.Buffer, peer []peer) (err error) {
+	for _, p := range peer {
+
+		ip4 := net.ParseIP(p.IP).To4()
+		log.Printf("ipv4: '% x' (%s)", ip4, ip4)
+		_, err = b.Write(ip4)
+		if err != nil {
+			return err
+		}
+
+		port, err := strconv.Atoi(p.Port)
+		if err != nil {
+			return err
+		}
+
+		portBytes := []byte{byte(port >> 8), byte(port)}
+
+		log.Printf("port: '% x' (%d)", portBytes, port)
+		_, err = b.Write(portBytes)
+		if err != nil {
+			return err
+		}
+	}
+	return err
 }
 
 //@todo implement
@@ -60,10 +97,106 @@ func (d *delugeProcess) Benchmark() error {
 		return err
 	}
 
+	tbind := ":9000"
+	go func() {
+
+		torrents := map[string]map[string]peer{}
+
+		log.Printf("Starting tracker on '%s'...", tbind)
+		err = http.ListenAndServe(tbind, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			//map[ipv6:[fe80::226:bbff:fe0a:e12b] info_hash:[?0FA??c??N???] left:[100000000] key:[B22982B1] event:[started] numwant:[200] compact:[1] no_peer_id:[1] peer_id:[-UM1870-{??qCzVB<ur] port:[40959] uploaded:[0] downloaded:[0] corrupt:[0]]
+			err := r.ParseForm()
+			if err != nil {
+				log.Printf("Failed to parse announce form data: %s", err)
+				return
+			}
+
+			host, port, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				log.Printf("Failed to parse remote host and port: %s", err)
+				return
+			}
+
+			if r.Form.Get("peer_id") == "" {
+				//@todo sometimes peer_id is empty?
+				return
+			}
+
+			//add announcing peer to peerlist of torrent
+			log.Printf("Torrent Client (peer_id: '%s', compact: %s) announces from %s:%s, port set to %s for info_hash '%x'", r.Form.Get("peer_id"), r.Form.Get("compact"), host, port, r.Form.Get("port"), r.Form.Get("info_hash"))
+			peerlist, ok := torrents[r.Form.Get("info_hash")]
+			if !ok {
+				peerlist = map[string]peer{}
+				torrents[r.Form.Get("info_hash")] = peerlist
+			}
+
+			peerlist[r.Form.Get("peer_id")] = peer{
+				PeerID: r.Form.Get("peer_id"),
+				IP:     host,
+				Port:   r.Form.Get("port"),
+			}
+
+			peers := []peer{}
+			for _, p := range peerlist {
+				peers = append(peers, p)
+			}
+
+			if r.Form.Get("compact") == "1" {
+				buff := bytes.NewBuffer(nil)
+				err = d.writeCompactPeers(buff, peers)
+				if err != nil {
+					log.Printf("Error: %s", err)
+					return
+				}
+
+				log.Printf("compact: '% x'", buff.Bytes())
+				data := struct {
+					Peers []byte `bencode:"peers"`
+				}{
+					Peers: buff.Bytes(),
+				}
+
+				//encode response
+				log.Printf("Returning announce with: %+v", data)
+				w.Header().Set("Content-Type", "text/plain")
+				enc := bencode.NewEncoder(w)
+				err = enc.Encode(data)
+				if err != nil {
+					log.Printf("Failed to bencode: %s", err)
+					return
+				}
+
+			} else {
+				data := struct {
+					Peers []peer `bencode:"peers"`
+				}{
+					Peers: peers,
+				}
+
+				//encode response
+				log.Printf("Returning announce with: %+v", data)
+				w.Header().Set("Content-Type", "text/plain")
+				enc := bencode.NewEncoder(w)
+				err = enc.Encode(data)
+				if err != nil {
+					log.Printf("Failed to bencode: %s", err)
+					return
+				}
+			}
+
+		}))
+
+		log.Printf("Tracker exited")
+		if err != nil {
+			log.Printf("Tracker failed: %s", err)
+		}
+	}()
+
 	//create a private torrent file for it, use a public tracker for announce (transmission-create)
 	tpath := f.Name() + ".torrent"
 	log.Printf("Creating .torrent file of '%s' ('%d' bytes) at '%s'...", f.Name(), size, tpath)
-	cmd := exec.Command("transmission-create", "-p", "-t=udp://tracker.openbittorrent.com:80", "-o", tpath, f.Name())
+	cmd := exec.Command("transmission-create", "-p", fmt.Sprintf("-t=http://%s%s/announce", d.ip.String(), tbind), "-o", tpath, f.Name())
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
@@ -84,7 +217,7 @@ func (d *delugeProcess) Benchmark() error {
 
 	//publish directory with torrent file on a simple file server
 	bind := ":8080"
-	log.Printf("Publishing torrent files on '%s'", bind)
+	log.Printf("Publishing torrent file at: 'http://%s%s/%s'", d.ip.String(), bind, filepath.Base(tpath))
 	go func() {
 		log.Fatal(http.ListenAndServe(bind, http.FileServer(http.Dir(dir))))
 	}()
